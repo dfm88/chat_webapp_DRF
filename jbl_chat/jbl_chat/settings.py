9f44d083c9aec851f02f9810b32875b0037aeb9f
dfm88 <diegof.university@gmail.com>
2022-02-26 17:05:45 +0100

    setup debug environment redis-celery - step 2

--- jbl_chat/jbl_chat/settings.py
+++ jbl_chat/jbl_chat/settings.py
@@ -133,3 +133,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redis and Celery Conf
+CELERY_BROKER_URL = "redis://redis:6379"
+CELERY_RESULT_BACKEND = "redis://redis:6379"
